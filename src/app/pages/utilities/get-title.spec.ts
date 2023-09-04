361ee2528ed03c0602736c5cf9b52ae6daecb068
uzihoon <uzihoon.dev@gmail.com>
2023-09-03 20:07:09 -0700

    Add Helmet

---
+++ src/app/pages/utilities/get-title.spec.ts
@@ -0,0 +1,8 @@
+import { getTitle } from './get-title';
+
+describe('Utility: getTitle', () => {
+  it('should return title', () => {
+    const title = 'EUN - test';
+    expect(getTitle('test')).toBe(title);
+  });
+});
